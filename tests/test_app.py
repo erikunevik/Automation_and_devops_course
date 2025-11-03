e6ef1b2066b38db8f6c3a33ef0c553e2a2c506bd
Erik Unevik <erikunevik@gmail.com>
2025-11-03 22:01:00 +0100

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,7 @@
+from app import add_one_and_one
+
+def test_add_one_and_one_returns_two():
+    assert add_one_and_one() == 2from app import add_one_and_one
+
+def test_add_one_and_one_returns_two():
+    assert add_one_and_one() == 2
