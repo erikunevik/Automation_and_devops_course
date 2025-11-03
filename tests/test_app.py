b216f5a9ab704056b22aad1c7cecfc296d8a5f35
Erik Unevik <erikunevik@gmail.com>
2025-11-03 22:17:33 +0100

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,4 @@
 from app import add_one_and_one
 
 def test_add_one_and_one_returns_two():
-    assert add_one_and_one() == 2 from app import add_one_and_one
-
-def test_add_one_and_one_returns_two():
     assert add_one_and_one() == 2
